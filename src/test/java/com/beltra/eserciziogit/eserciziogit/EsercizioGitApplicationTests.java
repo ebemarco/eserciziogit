e80827ed295abca85e026f9e5fb65fadec630793
Marco Beltrame <m.beltrame@reply.it>
2026-07-20 21:46:09 +0200

    Primo commit: il progetto si avvia correttamente.

---
+++ src/test/java/com/beltra/eserciziogit/eserciziogit/EsercizioGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beltra.eserciziogit.eserciziogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
